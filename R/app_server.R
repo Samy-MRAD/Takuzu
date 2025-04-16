1cc1f0351b26641799a5920728d9c28924479ec1
unknown <fabiancondamy@gmail.com>
2025-04-16 18:16:54 +0200

    màj boutons

--- R/app_server.R
+++ R/app_server.R
@@ -17,6 +17,9 @@ app_server <- function(input, output, session) {
 
   states <- c("", "0", "1")
 
+  # ReactiveValues utilisées dans la suite
+  compteur_help <- reactiveValues(remaining = 3)
+  compteur_verif <- reactiveValues(remaining = 1)
   state <- reactiveValues(values = matrix(rep("", total_buttons), nrow = nRows, ncol = nCols))
   solution <- reactiveValues(values = NULL)
   erreurs <- reactiveValues(indices = NULL)
@@ -27,6 +30,12 @@ app_server <- function(input, output, session) {
     solution$values <- grille_completee
     grille <- masquer_cases(grille_completee, nb_cases_vides[[niveau]])
     state$values <- grille
+
+    # compteurs d'utilisations restantes pour les boutons
+    compteur_help$remaining <- 3
+    compteur_verif$remaining <- 1
+    updateActionButton(session, "verif", label = "Vérifier la grille (1 restante)")
+    updateActionButton(session, "help", label = "Révéler une case (3 restantes)")
 
     # Activer le bouton "Révéler une case" après le lancement du niveau
     enable("help")
@@ -75,24 +84,46 @@ app_server <- function(input, output, session) {
   })
 
   observeEvent(input$help, {
-    empty_indices <- which(state$values == "", arr.ind = TRUE)
-    if (nrow(empty_indices) > 0) {
-      idx <- empty_indices[sample(1:nrow(empty_indices), 1), ]
-      row <- idx[1]
-      col <- idx[2]
-      state$values[row, col] <- solution$values[row, col]
+    if (compteur_help$remaining > 0) {
+      empty_indices <- which(state$values == "", arr.ind = TRUE)
+      if (nrow(empty_indices) > 0) {
+        idx <- empty_indices[sample(1:nrow(empty_indices), 1), ]
+        row <- idx[1]
+        col <- idx[2]
+        state$values[row, col] <- solution$values[row, col]
+
+        compteur_help$remaining <- compteur_help$remaining - 1
+
+        # Met à jour le label avec le nombre d'utilisations restantes
+        updateActionButton(session, "help",
+                           label = paste0("Révéler une case (", compteur_help$remaining, " restantes)"))
+
+        # Si on atteint 0, on désactive le bouton
+        if (compteur_help$remaining == 0) {
+          disable("help")
+        }
+      }
     }
   })
 
   observeEvent(input$verif, {
-    erreurs$indices <- NULL  # on vide les erreurs précédentes
+    if (compteur_verif$remaining > 0) {
+      erreurs$indices <- NULL  # on vide les erreurs précédentes
 
-    if (all(state$values == solution$values)) {
-      showModal(modalDialog("Bravo, grille correcte !", easyClose = TRUE))
-    } else {
-      diff_matrix <- state$values != solution$values
-      erreurs$indices <- which(diff_matrix, arr.ind = TRUE)
-      showModal(modalDialog("Il y a des erreurs", easyClose = TRUE))
+      if (all(state$values == solution$values)) {
+        showModal(modalDialog("Bravo, grille correcte !", easyClose = TRUE))
+      } else {
+        diff_matrix <- state$values != solution$values
+        erreurs$indices <- which(diff_matrix, arr.ind = TRUE)
+        showModal(modalDialog("Il y a des erreurs", easyClose = TRUE))
+      }
+
+      compteur_verif$remaining <- compteur_verif$remaining - 1
+      updateActionButton(session, "verif", label = paste0("Vérifier la grille (", compteur_verif$remaining, " restante)"))
+
+      if (compteur_verif$remaining == 0) {
+        disable("verif")
+      }
     }
   })
 }
